39ee2ca5856ce8d40858f2003caba0191ad7c2d6
Bhargava Teja <nbhargavteja2022@gmail.com>
2023-07-21 16:12:42 +0530

    updated promptCard and Readme file

--- myapp/README.md
+++ myapp/README.md
@@ -1,4 +1,5 @@
-create a Prompt sharing Page using nextjs 
-we can create, edit, delete & update the promts
-authentication using nextAuth 
-and many more  
+1) created a Prompt sharing Page using nextjs 
+2) we can create, edit, delete & update the promts
+3) authentication using nextAuth
+4) read and copy propmts created by other users
+5) Built using Nextjs & Tailwind 
--- myapp/components/PromtCard.jsx
+++ myapp/components/PromtCard.jsx
@@ -12,7 +12,10 @@ const PromtCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
 
   const [copied, setCopied] = useState("");
 
-  const handleProfileClick = () => {}
+  const handleProfileClick = () => {
+    if(post.creator._id === session?.user.id) return router.push("/profile");
+    router.push(`/profile/${post.creator._id}?name=${post.creator.username}`)
+  }
 
   const handleCopy = () => {
     setCopied(post.prompt);
